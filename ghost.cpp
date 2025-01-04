43d32e19267293b1247141bb07f2c6ca76b66aab
Anna Ber <annabe3@mta.ac.il>
2025-01-04 12:46:13 +0200

    ghost is not moving

--- ghost.cpp
+++ ghost.cpp
@@ -5,38 +5,31 @@ Ghost::Ghost(Point _pos, Key _dir, Board* _b)
     : pos(_pos), dir(_dir), board(_b) {}
 
 void Ghost::move() {
-    Point newPos = pos.neighbor(dir);
-   
+    
+    /*if (rand() % 100 < 5) {  // 5% chance to change direction
+        dir = LEFT;
+    }*/
 
+    /*Point new_pos = pos.neighbor(dir);//pos based on current direction
     char tileAtNewPos = board->getChar(newPos);
 
-    pos = newPos;
-
     
-   /**if (tileAtNewPos == '=' || tileAtNewPos == '<' || tileAtNewPos == '>') {
-        
-        pos = newPos;
+    if (tileAtNewPos == ' ') {
+        pos = new_pos;
     }
     else {
-        // If it's not a floor, reverse direction and check again
-        reverseDirection(); // Ensure this properly updates the direction
-        newPos = position.neighbor(direction);
-
-        // Check again if the new position is valid
-        tileAtNewPos = board->getChar(newPos);
-        if (tileAtNewPos == '=' || tileAtNewPos == '<' || tileAtNewPos == '>') {
-            position = newPos;  // Update the position after reversing direction
-        }
-    }
-
-    // Optional: Check if ghost encounters another ghost
-    /*for (auto& ghost : board->getGhosts()) {
-        if (ghost.position == position) {
-            changeDirection();
-            ghost.changeDirection();
-        }
+        reverseDirection();
     }*/
+	
+	
+    Point new_pos = pos.neighbor(dir);
+
+	pos = new_pos;
+
+	
+
 }
+
 
 bool Ghost::detectMario(Point marioPos) {
     return pos == marioPos; 
@@ -44,12 +37,14 @@ bool Ghost::detectMario(Point marioPos) {
 
 void Ghost::reverseDirection() {
     
-    if (dir == UP) dir = DOWN;
-    else if (dir == DOWN) dir = UP;
-    else if (dir == LEFT) dir = RIGHT;
-    else if (dir == RIGHT) dir = LEFT;
-   
+    if (dir == LEFT) {
+        dir = RIGHT;
+    }
+    else if (dir == RIGHT) {
+        dir = LEFT;
+    }
 }
+
 
 void Ghost::draw() const {
     board->drawChar('X', pos);
