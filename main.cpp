0031beb283f3768b1d8e7bf312c50e75e53081bc
Sean Berger <seanbe@mta.ac.il>
2025-01-24 19:56:51 +0200

    changed dkong.exe directory

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@
 
 int main()
 {	
+	HWND console = GetConsoleWindow();
+	RECT r;
+	GetWindowRect(console, &r); //stores the console's current dimensions
+
+	MoveWindow(console, r.left, r.top, 80, 25, TRUE); // 80 width, 25 height
+
 	Menu menu;
 	menu.display();
 }
